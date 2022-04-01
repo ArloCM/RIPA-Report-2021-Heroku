74a9309f9fe605088cef80a47910750d02706f85
Arlo <arlo.c.malmberg@gmail.com>
2022-04-01 11:33:50 -0700

    footer

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ hide_decoration_bar_style = '''
     <style>
         header {visibility: hidden;}
         footer {visibility: hidden;}
+        .reportview-container .main footer {visibility: hidden;}    
     </style>
 '''
 st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
