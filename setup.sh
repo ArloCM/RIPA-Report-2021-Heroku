1c525563cc849996b9e530993a77f68140069e8f
Arlo Malmberg <arlo.c.malmberg@gmail.com>
2022-04-15 10:21:22 -0700

    config

--- setup.sh
+++ setup.sh
@@ -5,4 +5,9 @@ headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
+[theme]\n\
+base = 'light'\n\
+primaryColor = '#3b7b9b'\n\
+secondaryBackgroundColor = '#f0f0f0'\n\
+textColor = '#0d1d29'\n\
 " > ~/.streamlit/config.toml
